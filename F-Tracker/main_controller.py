f24bf78f31108f4750bb19d703c8291d3648da08
Theuns Robert Pretorius <thns.pretorius@gmail.com>
2023-08-30 21:05:16 +0200

    Update 2023/08/30
    This update contains a security layer, where the user can register or login. also contains a special root code, only for developer personal use.

--- F-Tracker/main_controller.py
+++ F-Tracker/main_controller.py
@@ -4,9 +4,12 @@ exist, and then starts up the program'''
 #=====Imports====#
 from Business_Layer.database_connect import Database_Connection
 from Presentation_layer.user_login import User_login
-import Presentation_layer.shared_display
+
 
 #=====Run Program====#
 
+# Establishes database connection
 test = Database_Connection()
+
+# Opens Login options
 login = User_login()
